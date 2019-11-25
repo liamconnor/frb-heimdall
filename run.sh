0d41d922bc59fdca1db2b9721356b25f92799563
Tom Klaver <tomklav@gmail.com>
2019-11-25 11:13:33 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ INPUT_PATH="/data/input/test_data"
 OUTPUT_PATH="/data/output"
 mkdir -p $TMP_PATH
 heimdall -v -f $INPUT_PATH -dm 10. 3000. -rfi_no_narrow -rfi_no_broad -output_dir $TMP_PATH
-# cols: DM, S/N, TIME, LOG_2_DOWNSAMPLE
+# cols: DM, S/N, TIME, LOG_2_DOWNSAMPLE, FREQ_REF
 cat $TMP_PATH/*.cand | awk 'BEGIN{FS=" "} { print $6 " " $1 " " $3 " " 2^$4 " " 1536.00 }' > $OUTPUT_PATH
